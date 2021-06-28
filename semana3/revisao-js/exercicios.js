edf451125fd4f0f53991bcad3df9782e2eebd36e
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-06-28 10:04:33 -0300

    Exercício 6 concluído

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -47,7 +47,8 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
-
+  const respostas = [false, false, true, true, true]
+  return respostas
 }
 
 // EXERCÍCIO 07
