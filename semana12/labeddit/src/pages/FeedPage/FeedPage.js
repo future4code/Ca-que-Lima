4c0262ef0b52c87f3182bc3b02b604c771a9a892
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-09-01 16:19:01 -0300

    Implementado o Protected Page

--- semana12/labeddit/src/pages/FeedPage/FeedPage.js
+++ semana12/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useProtectedPage } from '../../hooks/useProtectedPage'
 
 export default function FeedPage() {
+
+    useProtectedPage()
+    
     return (
         <div>
             
--- semana12/labeddit/src/pages/PostPage/PostPage.js
+++ semana12/labeddit/src/pages/PostPage/PostPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useProtectedPage } from '../../hooks/useProtectedPage'
 
 export default function PostPage() {
+
+    useProtectedPage()
+    
     return (
         <div>
             
