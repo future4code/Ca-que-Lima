f151e40f7f7cfeeb0e7ae1f67150662fd90d472a
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-09-02 17:28:17 -0300

    Iniciado card de posts\

--- semana12/labeddit/src/pages/FeedPage/FeedPage.js
+++ semana12/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import PostCard from '../../components/Card/Card'
 import { useProtectedPage } from '../../hooks/useProtectedPage'
+import { ScreenContainer } from './styles'
 
 export default function FeedPage() {
 
     useProtectedPage()
     
     return (
-        <div>
-            <h1>Feed</h1>
-        </div>
+        <ScreenContainer>
+            <PostCard />
+        </ScreenContainer>
     )
 }
--- semana12/labeddit/src/pages/FeedPage/styles.js
+++ semana12/labeddit/src/pages/FeedPage/styles.js
@@ -1,1 +1,9 @@
 import styled from 'styled-components'
+
+export const ScreenContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    align-items: center;
+    margin: 30px auto;
+`
