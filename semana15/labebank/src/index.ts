6a2b5dd4fb2c84c502a858876f11fbdbb4980768
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-09-24 11:14:14 -0300

    tipo user definido

--- semana15/labebank/src/index.ts
+++ semana15/labebank/src/index.ts
@@ -5,10 +5,29 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+type User = {
+    name: string,
+    cpf: number,
+    birthDate: string
+}
+
+const users: User[] = [
+    {
+        name: "Caíque",
+        cpf: 11122233345,
+        birthDate: "20/05/1994"
+    },
+    {
+        name: "Naiara",
+        cpf: 12312312345,
+        birthDate: "28/01/1992"
+    }
+]
+
 app.listen(3003, () => {
     console.log('Server running on port 3003')
 })
 
-app.get("/ping", (req: Request, res: Response) => {
-    res.status(200).send("pong!")
+app.get("/users", (req: Request, res: Response) => {
+    res.status(200).send(users)
 })
