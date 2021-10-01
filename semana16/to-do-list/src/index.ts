54d3640a0c50c41defe987571405780690b0bc30
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-10-01 13:53:58 -0300

    Endpoint de editar usuário implementado, e alterado o objeto retornado de get user by id

--- semana16/to-do-list/src/index.ts
+++ semana16/to-do-list/src/index.ts
@@ -13,35 +13,56 @@ app.get("/users", async (req: Request, res: Response): Promise<any> => {
         const users = await connection("ToDoListUser")
         res.status(200).send(users)
     } catch (error: any) {
-        res.send(error.message || error.sqlMessage)
+        res.send(error.sqlMessage || error.message)
     }
 })
 
 app.get("/users/:id", async (req: Request, res: Response): Promise<any> => {
+    const id: number = Number(req.params.id)
     try {
-        const id: number = Number(req.params.id)
-        const users = await connection("ToDoListUser").where({ id: id })
-        res.status(200).send(users)
+        const user = await connection("ToDoListUser").where({ id: id })
+        res.status(200).send({ id: user[0].id, nickname: user[0].nickname })
     } catch (error: any) {
-        res.send(error.message || error.sqlMessage)
+        res.send(error.sqlMessage || error.message)
     }
 })
 
 app.post("/users", async (req: Request, res: Response): Promise<any> => {
+    let errorCode = 400
     try {
         const { name, nickname, email } = req.body
         if (!name || !nickname || !email) {
-            res.statusCode = 400
             throw new Error('Por favor preencha todos os campos')
         } else {
-            await connection("ToDoListUser").insert({ name: name, nickname: nickname, email: email})
+            await connection("ToDoListUser").insert({ name: name, nickname: nickname, email: email })
             res.status(200).send('Usuário inserido com sucesso')
         }
     } catch (error: any) {
-        res.send(error.message || error.sqlMessage)
+        res.status(errorCode).send(error.sqlMessage || error.message)
     }
 })
 
+app.put("/users/edit/:id", async (req: Request, res: Response): Promise<any> => {
+    let errorCode = 400
+    const id: number = Number(req.params.id)
+    const { name, nickname } = req.body
+
+    try {
+        if (name === "" || nickname === "") {
+            throw new Error('Por favor cheque os campos')
+        } else {
+            if (name) {
+                await connection("ToDoListUser").where({ id: id }).update({ name: name })
+            }
+            if (nickname) {
+                await connection("ToDoListUser").where({ id: id }).update({ nickname: nickname })
+            }
+            res.status(200).send('Usuário atualizado com sucesso')
+        }
+    } catch (error: any) {
+        res.status(errorCode).send(error.sqlMessage || error.message)
+    }
+})
 
 
 
