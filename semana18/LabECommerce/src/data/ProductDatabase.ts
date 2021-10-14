7998d4bd281a2a10ff8c0c8ab43868165253de44
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-10-14 14:39:41 -0300

    create product implementado

---
+++ semana18/LabECommerce/src/data/ProductDatabase.ts
@@ -0,0 +1,18 @@
+import { BaseDatabase } from "./BaseDatabase"
+import { Product } from "../entities/Product"
+
+const productsTable: string = "labecommerce_products"
+
+export class ProductDatabase extends BaseDatabase {
+    public async create(product: Product) {
+        await BaseDatabase.connection(productsTable).insert(product)
+    }
+
+    public async getAll() {
+        return BaseDatabase.connection(productsTable)
+    }
+
+    public async deleteById(id: string) {
+        await BaseDatabase.connection(productsTable).delete().where({ id })
+    }
+}
