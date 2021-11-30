9f070c2634775c572ef05487d4881e10413fe051
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-11-30 15:07:49 -0300

    exercício concluído

---
+++ semana25/logic-exercises/src/exercise-2.ts
@@ -0,0 +1,35 @@
+function soleNumber1(nums: number[]): number {
+    let result = 0
+    for (const num of nums) {
+        result ^= num
+    }
+    return result
+}
+
+// ----------------------------------------------------
+
+
+interface HashTable {
+    [key: string]: any
+}
+
+function soleNumber2(nums: number[]): number {
+    const hash: HashTable = {}
+    
+    for (let num of nums) {
+        const key = num
+        
+        if (key in hash) {
+            delete hash[key]
+        } else {
+            hash[key] = true
+        }
+    }
+    
+    return Number(Object.keys(hash)[0])
+}
+
+
+
+console.log('XOR: ', soleNumber1([4,1,2,1,2]))
+console.log('Hash Table: ', soleNumber2([4,1,2,1,2]))
